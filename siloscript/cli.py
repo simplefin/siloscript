18abc994cc953ed037d5b245241982026e702faf
Matt Haggard <haggardii@gmail.com>
2015-04-16 22:09:10 -0600

    add missing .path

--- siloscript/cli.py
+++ siloscript/cli.py
@@ -33,7 +33,7 @@ server_parser.add_argument('--scripts', '-s',
     default=root.child('data').child('scripts').path,
     help='Path to executable scripts.  (default: %(default)s)')
 server_parser.add_argument('--static-root', '-S',
-    default=root.child('data').child('static'),
+    default=root.child('data').child('static').path,
     help='Path to static files served at /static.  (default: %(default)s)')
 server_parser.set_defaults(func=serve)
 
